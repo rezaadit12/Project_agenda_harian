0a059ff1b977a82bf9c4e1a61145fca668f36da6
raichan07 <raichandintar@gmail.com>
2023-09-21 09:52:40 +0700

    Login

---
+++ Login/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="center">
+        <h1>Login</h1>
+        <form action="" method="post">
+            <div class="txt_field">
+                <input type="text" required>
+                <span></span>
+                <i class='bx bxs-user'></i>
+                <label>Username</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" required>
+                <span></span>
+                <i class='bx bxs-lock-alt'></i>
+                <label>Password</label>
+            </div>
+            <input type="submit" value="Login">
+            <div class="signup_link">
+                Not a member? <a href="#">Sign up</a>
+            </div>
+        </form>
+        <img class= "study" src="working1.png" alt="img" >
+    </div>
+</body>
+</html>
